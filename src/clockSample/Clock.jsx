6f777f7a4d4dd267d47d914f24c5d88c526c3210
yjshin2 <yjshin2@enliple.com>
2022-12-13 14:26:01 +0900

    현재 시간 useState, useEffect, setInterval 사용하는 방식으로 수정

--- src/clockSample/Clock.jsx
+++ src/clockSample/Clock.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
+import React, {useEffect, useRef, useState} from "react";
 
 function Clock(props) {
-    // const thisTime = setInterval(() => {
-    // Clock.render(
-    //         <Clock/>
-    //     );
-    // }, 1000);
+    let [time, setTimer] = useState(new Date().toLocaleTimeString());
+
+    useEffect(() => {
+        setInterval(() => {
+            setTimer(new Date().toLocaleTimeString());
+        }, 1000);
+    },[]);
+
     const innerColckStyle = {
         backgroundColor: "#282c34",
         minHeight: "154px",
@@ -17,20 +18,14 @@ function Clock(props) {
         justifyContent: "center",
         fontSize: "calc(10px + 2vmin)",
         color: "white",
-    }
-    const time =  (
-        <div style={innerColckStyle}>
-            <h1>{new Date().toLocaleTimeString()}</h1>
-        </div>
-    );
+    };
 
     return (
         <div style={innerColckStyle}>
-            <h1>{new Date().toLocaleTimeString()}</h1>
+            <h1>{time}</h1>
         </div>
     );
 
-    //root.render(time);
 }
 
 //setInterval(Clock, 1000);
