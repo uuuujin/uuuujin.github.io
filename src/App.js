c2128f36cc57f517b557e26f44b4101d84299da6
yjshin2 <yjshin2@enliple.com>
2022-12-13 11:50:01 +0900

    Home 화면 animate button 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './animate/MoveButton';
+import MoveButton from "./animate/MoveButton";
 
 
 function App() {
-
-
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MoveButton/>
         <h1>
           this is uuuujin github
         </h1>
-          <p>my react 연습장</p>
+          <p>나의 개발 연습장</p>
         <a
           className="App-link"
           href="https://syz.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          time is
+          syz blog
         </a>
       </header>
     </div>
