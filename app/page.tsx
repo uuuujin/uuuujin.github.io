5e3c87ef090c3971b87e6d896bebb1be405445de
shin-yujin-mac <in1006sh@gmail.com>
2024-11-01 01:19:10 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <button>
-                    <Link href="/music">music</Link>
-                </button>
+               here is nothing
             </div>
         </main>
     );
