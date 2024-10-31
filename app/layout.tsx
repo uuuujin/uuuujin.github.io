16bb2491c4cec555a4adfa998b9586dd8b026f28
shin-yujin-mac <in1006sh@gmail.com>
2024-11-01 00:37:07 +0900

    deploy 젭알 되라

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "😘😘😘😘😘😘😘😘😘",
+  description: "😘",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
